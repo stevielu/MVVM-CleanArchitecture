462673d96026c1f69f1cdaa9289d21f7200aec62
stevie <luwei@huali-tec.com>
2019-03-19 18:04:01 +0800

    Add Error Class

---
+++ QiWi/BaseClass/Error/ErrorObject.swift
@@ -0,0 +1,12 @@
+//
+//  ErrorObject.swift
+//  QiWi
+//
+//  Created by stevie on 2019/3/19.
+//  Copyright © 2019 IQQL. All rights reserved.
+//
+
+import Foundation
+class ErrorObject: <#super class#> {
+    <#code#>
+}
