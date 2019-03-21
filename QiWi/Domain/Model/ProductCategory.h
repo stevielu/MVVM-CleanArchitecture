0248981de57fffa07f8920e9e48d2662f8975fc5
stevie <luwei@huali-tec.com>
2019-03-21 18:06:51 +0800

    model category

--- QiWi/Domain/Model/ProductCategory.h
+++ QiWi/Domain/Model/ProductCategory.h
@@ -8,10 +8,18 @@
 
 #import "HLValueObject.h"
 
-NS_ASSUME_NONNULL_BEGIN
-
-@interface ProductCategory : HLValueObject
+@protocol ProductCategory <NSObject>
 
 @end
 
-NS_ASSUME_NONNULL_END
+@interface ProductCategory : HLValueObject
+@property (nonatomic, copy, nullable) NSString *name;
+@property (nonatomic, copy, nullable) NSString *nid;
+@property (nonatomic, copy, nullable) NSNumber *code;
+@property (nonatomic, copy, nullable) NSNumber *desc;
+@property (nonatomic, copy, nullable) NSNumber *category_type;
+@property (nonatomic, copy, nullable) NSNumber *parent_category;
+@property (nonatomic, copy, nullable) NSArray <ProductCategory *> *sub_cat;
+@end
+
+
