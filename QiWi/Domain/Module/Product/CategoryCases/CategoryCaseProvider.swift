b6e1bdc1a5a87d3c6c77468dac5015a6d69b5fd9
stevie <luwei@huali-tec.com>
2019-03-21 18:07:20 +0800

    domain case provider

---
+++ QiWi/Domain/Module/Product/CategoryCases/CategoryCaseProvider.swift
@@ -0,0 +1,12 @@
+//
+//  CategoryCaseProvider.swift
+//  QiWi
+//
+//  Created by stevie on 2019/3/21.
+//  Copyright © 2019 IQQL. All rights reserved.
+//
+
+import Foundation
+public protocol CategoryCase {
+    func get() ->
+}
